e3995792018d11dab0a615a96274120f33c9b5e9
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-01 20:55:37 +1000

    added content to the data props to render the list - needs more styling

--- src/Components/Info/Data/Data.js
+++ src/Components/Info/Data/Data.js
@@ -8,7 +8,7 @@ function Data() {
         <div className="data-info-container">
             <Landing />
             <Information title="Built for your ideas" description="lorem"/>
-            <Facts title="Allowing businesses to leverage the power of data" description="The world revolves around data, and our data is really... really good. Get live price feeds for markets, technical indicators information for historical periods and much more with the live market data package." list={[1,2,3,4,5]}/>
+            <Facts title="Allowing businesses to leverage the power of data" description="The world revolves around data, and our data is really... really good. Get live price feeds for markets, technical indicators information for historical periods and much more with the live market data package." list={["Comprehensive market data across multiple markets including cryptocurrencies and traditional markets.", "Technical indicator data such as moving averages", "Support for integration into external application via our API", "Enables a user to leverage live data feeds to build their applications around", "Empowers users to maximize efficiency by building strategies around historical and live data"]}/>
         </div>
     );
 }
--- src/Components/Info/Reusable/Facts.js
+++ src/Components/Info/Reusable/Facts.js
@@ -5,6 +5,7 @@ import "../../../Assets/Styles/Info/Data/facts.css";
 function Facts(props) {
     return (
         <div className="facts-container">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <div className="facts-left">
                 <div className="facts-content">
                     <h1 className="facts-title">{props.title}</h1>
@@ -12,7 +13,7 @@ function Facts(props) {
                     <ul className="facts-list-container">
                         {
                             props.list.map((value, index) => {
-                                return <li className="fact-list-item">{value}</li>
+                                return <li className="fact-list-item"><i class="fa-solid fa-diamond"></i>{value}</li>
                             })
                         }
                     </ul>
