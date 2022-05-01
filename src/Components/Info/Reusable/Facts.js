70345f6e7984abcccf44c607c03432546b426675
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-01 20:29:10 +1000

    added looping functinoality to handle different lengths of lists passed as props

--- src/Components/Info/Reusable/Facts.js
+++ src/Components/Info/Reusable/Facts.js
@@ -3,24 +3,19 @@ import "../../../Assets/Styles/Info/Data/facts.css";
 
 
 function Facts(props) {
-    // function renderList() {
-    //     const listContainer = document.querySelector(".facts-list-container");
-    //     const list = props.list;
-    //     console.log(list.length);
-    //     for (const item in list) {
-    //         const listItem = document.createElement("li");
-    //         listItem.className = "fact-list-item";
-    //         listItem.textContent = item;
-    //         listContainer.appendChild(listItem);
-    //     }
-    // }
     return (
         <div className="facts-container">
             <div className="facts-left">
                 <div className="facts-content">
                     <h1 className="facts-title">{props.title}</h1>
                     <p className="facts-description">{props.description}</p>
-                    <ul className="facts-list-container"></ul>
+                    <ul className="facts-list-container">
+                        {
+                            props.list.map((value, index) => {
+                                return <li className="fact-list-item">{value}</li>
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
