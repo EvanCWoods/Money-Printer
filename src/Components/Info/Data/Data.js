be83915e5587cee485e8b4760430f89550322ca3
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-01 19:28:13 +1000

    added a resuable folder to store all components for the information pages to be reused each time with props controlling information

--- src/Components/Info/Data/Data.js
+++ src/Components/Info/Data/Data.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Landing from "./Landing/Landing.js";
 
 function Data() {
     return( 
         <div className="data-info-container">
-            test
+            <Landing />
         </div>
     );
 }
---
+++ src/Components/Info/Data/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Reusable/Header.js";
+
+function Landing() {
+    return (
+        <div className="data-info-landing-container">
+            <Header top="Live market" bottom="data"/>
+        </div>
+    );
+}
+
+export default Landing;
