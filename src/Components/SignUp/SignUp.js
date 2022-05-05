92411fb950827a0868d0ca2f75c5e0fd8f2767d6
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-05 18:10:40 +1000

    added changes

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -2,25 +2,47 @@ import React from "react";
 import Header from "../Reusable/Header.js"
 import "../../Assets/Styles/signUp/signUp.css";
 
+
 function SignUp() {
+    function sendForm(event) {
+        event.preventDefault();
+        const firstName = document.querySelector(".firstName").value;
+        const lastName = document.querySelector(".lastName").value;
+        const email = document.querySelector(".email").value;
+        const password = document.querySelector(".password").value;
+        const confirmPassword = document.querySelector(".confirmPassword").value;
+
+        if (password === confirmPassword) {
+            fetch("/api/users/create", {
+                method: "POST",
+                body: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                }
+            });
+        }
+    }
     return (
         <div className="sign-up-container">
-            <Header top="Sign up to" bottom="the future" />
+            <Header top="Unlock" bottom="the future" />
             <div className="sign-up-form-container">
-                <form className="sign-up-form">
+                <form className="sign-up-form" onSubmit={sendForm}>
                     <div className="names-container">
-                        <input type="text" className="sign-up-input input" placeholder="First Name"></input>
-                        <input type="text" className="sign-up-input input" placeholder="Last Name"></input>
+                        <input type="text" className="sign-up-input input firstName" placeholder="First Name"></input>
+                        <input type="text" className="sign-up-input input lastName" placeholder="Last Name"></input>
                     </div>
                     <div className="email-container">
-                        <input type="text" className="sign-up-email input" placeholder="Email"></input>
+                        <input type="text" className="sign-up-email input email" placeholder="Email"></input>
                     </div>
                     <div className="password-container">
-                        <input type="text" className="sign-up-password input" placeholder="Password"></input>
-                        <input type="text" className="sign-up-password input" placeholder="Confirm Password"></input>
+                        <input type="text" className="sign-up-password input password" placeholder="Password"></input>
+                        <input type="text" className="sign-up-password input confirmPassword" placeholder="Confirm Password"></input>
                     </div>
                     <div className="sign-up-button-container">
-                        <button className="sign-up-button">Confirm</button>
+                        
+                        <button className="sign-up-button" type="submit">Confirm</button>
                     </div>
                 </form>
             </div>
