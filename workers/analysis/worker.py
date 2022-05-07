1363c606e677ccb99da57e111d35b586fa536f9c
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-07 20:09:56 +1000

    added logic to handle initial addition to the new database

--- workers/analysis/worker.py
+++ workers/analysis/worker.py
@@ -1,12 +1,14 @@
 # IMPORT LIBRARIES
 from decouple import config
 from pymongo import MongoClient
-import os
 import ssl
+import pandas as pd
+import numpy as np
 
 # GLOBAL VARIABLES
 HOUR = 3600
 RAW_MONGO_URI = config("RAW_MONGO_URI")
+RATE = 200
 
 
 # FUNCTION TO LOOP THROUGH ALL DATABASE SCHEMAS
@@ -22,13 +24,34 @@ def getSchemas(schema):
 def analysis():
     schemaList = ["live", "ethereum", "binance"]    # LIST THE SCHEMAS TO BE LOOPED
     for element in schemaList:
-        data = list(getSchemas(element).find().sort("timestamp", 1))    # FOR EACH SCHEMA, GET THE DATA IN ASCENDING TIME ORDER
-        newObject = {
-            "Timestamp": data.timestamp,
-            "Ticker": element,
-            "Price": data.Close,
-        }
+        data = list(getSchemas("live").find().sort("timestamp", 1))    # FOR EACH SCHEMA, GET THE DATA IN ASCENDING TIME ORDER
+        
+        # LOOP THROUGH AND ISOLATE THE CLOSING PRICE
+        prices = []
+        timestamps = []
+        signals = []
+        for item in data:
+            prices.append(item["Close"])  
+            timestamps.append(item["timestamp"])  
 
+        averages = getMa(pd.DataFrame(prices), RATE)    # GET THE AVERAGE
+        prices = np.array(prices[199:]).flatten()  # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
+        timestamps = np.array(timestamps[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
+        averages = np.array(averages[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
+        
+        i = 0
+        while i < len(prices):
+            if (prices[i] > averages[i]):
+                signals.append("Buy")
+            else:
+                signals.append("Sell")
+            i+=1
+
+# FUNCTION TO GET AVERAGE
+def getMa(prices, rate):
+    return prices.rolling(rate).mean()
+
+# FUNCTION TO RUN THE ALGORITHM
 
 
 # FUNCTION TO SEND THE NEW DATA TO THE NEW DATABASE
