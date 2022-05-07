e60e1d724a595039d89be0cd0f40d6007096f8cb
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-08 08:45:18 +1000

    added a new worker file to run hourly -> need to implatment timer.

--- workers/analysis/worker.py
+++ workers/analysis/worker.py
@@ -1,27 +1,14 @@
-# IMPORT LIBRARIES
 from decouple import config
 from pymongo import MongoClient
 import ssl
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-
-### 
-# NOTES FOR TOMORROW:
-    # THIS ONLY APPLIES TO BTC DATA RIGHT NOW SO CAN CURATE THIS TO BE ONE TIME USE SCRIPT
-        # GLOBAL VARIABLES THAT ARE EASY TO CHANGE FOR EACH COINTS
-    
-    # SET UP WORKER TO RUN THIS EVERY HOUR BEFORE ADDING DATA TO THE DATABASE
-
-###
-
-# GLOBAL VARIABLES
 HOUR = 3600
 RAW_MONGO_URI = config("RAW_MONGO_URI")
 RATE = 200
 
 
-# FUNCTION TO LOOP THROUGH ALL DATABASE SCHEMAS
 def getSchemas(schema):
     cluster = MongoClient(RAW_MONGO_URI,
             ssl_cert_reqs=ssl.CERT_NONE)
@@ -29,72 +16,54 @@ def getSchemas(schema):
     return db[schema]
 
 
-# FUNCTION TO PERFORM ANALYSIS ON THE DATA FORM EACH SCHEMA
-    # RETURN AN OBJECT WITH TIMESTAMP, AVERAGES, TICKER, PRICE, BUY/SELL ETC.
+
 def analysis():
-    schemaList = ["live", "ethereum", "binance"]    # LIST THE SCHEMAS TO BE LOOPED
-    for element in schemaList:
-        data = list(getSchemas("live").find().sort("timestamp", 1))    # FOR EACH SCHEMA, GET THE DATA IN ASCENDING TIME ORDER
+    data = list(getSchemas("live").find().sort("timestamp", 1))    # FOR EACH SCHEMA, GET THE DATA IN ASCENDING TIME ORDER
         
-        # LOOP THROUGH AND ISOLATE THE CLOSING PRICE
-        prices = []
-        timestamps = []
-        signals = []
-        for item in data:
-            prices.append(item["Close"])  
-            timestamps.append(item["timestamp"])  
+    # LOOP THROUGH AND ISOLATE THE CLOSING PRICE
+    prices = []
+    timestamps = []
+    signals = []
+    for item in data:
+        prices.append(item["Close"])  
+        timestamps.append(item["timestamp"])  
+    
+    prices = prices[-200:]
+    timestamps = timestamps[-200:]
+    
 
-        averages = getMa(pd.DataFrame(prices), RATE)    # GET THE AVERAGE
-        prices = np.array(prices[199:]).flatten()  # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
-        timestamps = np.array(timestamps[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
-        averages = np.array(averages[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
-        
-        i = 0
-        while i < len(prices):
-            if (prices[i] > averages[i]):
-                signals.append("Buy")
-            else:
-                signals.append("Sell")
-            i+=1
+    averages = getMa(pd.DataFrame(prices), RATE)    # GET THE AVERAGE
+    prices = np.array(prices[199:]).flatten()  # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
+    timestamps = np.array(timestamps[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
+    averages = np.array(averages[199:]).flatten()   # ADJUST FOR THE LOSS IN AVERAGE CALCULATION
 
-        # CREATE AND ADD NEW OBJECTS TO DATABASE
-        forDB = []
+    
+    if (prices > averages):
+        signals.append("Buy")
+    else:
+        signals.append("Sell")
 
-        n = 0
-        while n < len(prices):
-            # RE ASSIGN THE TICKER VALUE
-            if (element == "live"):
-                ticker = "BTC"
-            elif (element == "ethereum"):
-                ticker = "ETH"
-            elif (element == "binance"):
-                ticker = "BNB"
+    # CREATE AND ADD NEW OBJECTS TO DATABASE
+   
+    # RE ASSIGN THE TICKER VALUE
+    ticker = "BTC"
 
-            forDB.appen(
-                {
-                    "Timestamp": timestamps[n],
-                    "Ticker": ticker,
-                    "Close": prices[i],
-                    "MA200": averages[i],
-                    "Signal": signals[i]
-                }
-            )
+    print(
+        {
+            "Timestamp": timestamps[-1],
+            "Ticker": ticker,
+            "Close": prices[-1],
+            "MA200": averages[-1],
+            "Signal": signals[-1]
+            }
+        )
+    
+
+
 
 # FUNCTION TO GET AVERAGE
 def getMa(prices, rate):
     return prices.rolling(rate).mean()
-
-# FUNCTION TO RUN THE ALGORITHM
-
-
-# FUNCTION TO SEND THE NEW DATA TO THE NEW DATABASE
-    # MULTIPLE SCHEMAS
-
-
-# RUN THE WORKER EVERY HOUR, 5 SECONDS AFTER THE NEW HOUR, TO ENSURE NEW DATA IS IN ALL SCHEMAS.
-
-
-
 
 
 def main():
