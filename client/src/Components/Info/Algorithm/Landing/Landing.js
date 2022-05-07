13a80be24a7a3271f560bcdeee22d523a6ef4c3a
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-07 17:04:39 +1000

    added content to the algorithms information page

---
+++ client/src/Components/Info/Algorithm/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../../Reusable/Header.js";
+
+function Landing() {
+    return (
+        <div className="data-info-landing-container">
+            <Header top="Top performing " bottom="algorithms"/>
+        </div>
+    );
+}
+
+export default Landing;
