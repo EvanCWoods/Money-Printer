8e3dcd13efd84dd608f63dd3748c5fc056c726ea
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-16 21:21:06 +1000

    final fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoConnection = require('./config/connection');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/build')));
