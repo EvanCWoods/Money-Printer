33d034e22070e9445bc667366834b3db596fabbb
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-16 21:47:25 +1000

    added new get

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,16 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(express.json());
 app.use(routes);
 
 mongoConnection();
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}!`);
 });
