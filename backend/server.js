8e948b90079cc9208b907629fe171cdf280117af
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-16 22:00:37 +1000

    levina helped

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,21 @@ const mongoConnection = require('./config/connection');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 app.use(routes);
 
 mongoConnection();
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}!`);
