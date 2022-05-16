ab1715d3820e386f7a77a3a613585c1b5ac8156b
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-16 21:06:28 +1000

    fixed port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const mongoConnection = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
