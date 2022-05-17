be90d914ab5ec1ff5b55e23d90a222afa044de73
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-05-17 18:54:48 +1000

    added hardcoded data to demo the full dashboard

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -35,6 +35,9 @@ function Dashboard() {
     return(
         <div className="dashboard-container">
             <Card color={data.Signal} ticker={data.Ticker} price={`$${data.LastClose}`} marketSide={data.Signal} currentPercentage={`${data.Performance}%`} duration={`${data.Duration} Days`} Recomendation={recomendation}/>
+            <Card color="Buy" ticker="AAPL" price="$199.23" marketSide="Buy" currentPercentage="12%" duration="41 Days" Recomendation="Hold"/>
+            <Card color="Sell" ticker="TSLA" price="$724.37" marketSide="Sell" currentPercentage="-1.3%" duration="1 Days" Recomendation="Hold"/>
+            <Card color="Sell" ticker="ETH" price="$2071" marketSide="Sell" currentPercentage="36%" duration="19 Days" Recomendation="Buy"/>
         </div>
     );
 }
