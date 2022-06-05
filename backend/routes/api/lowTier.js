0c352a0631e549d1097f67031fab1588563776fd
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-06-05 15:26:29 +1000

    added another api endpoint

--- backend/routes/api/lowTier.js
+++ backend/routes/api/lowTier.js
@@ -4,7 +4,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-router.get("/btc", async (req, res) => {
+router.get("/all/btc", async (req, res) => {
+    console.log(req.headers);
   if (req.headers.apikey) {
     const client = new MongoClient(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -13,12 +14,12 @@ router.get("/btc", async (req, res) => {
     const apiCheck = await client
       .db("users")
       .collection("users")
-      .findOne({ apikey: req.headers.apiKey });
+      .findOne({ apiKey: req.headers.apikey });
 
     if (apiCheck) {
       const cursor = await client
-        .db("data")
-        .collection("live")
+        .db("algorithm")
+        .collection("bitcoin")
         .find()
         .sort({ Timestamp: 1 });
       const results = await cursor.toArray();
@@ -30,5 +31,34 @@ router.get("/btc", async (req, res) => {
     res.json("You must provide a valid API key in the request headers.");
   }
 });
+
+
+router.get("/current/btc", async (req, res) => {
+    if (req.headers.apikey) {
+      const client = new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+      });
+      await client.connect();
+      const apiCheck = await client
+        .db("users")
+        .collection("users")
+        .findOne({ apiKey: req.headers.apikey });
+  
+      if (apiCheck) {
+        const cursor = await client
+          .db("algorithm")
+          .collection("bitcoin")
+          .find()
+          .sort({ Timestamp: 1 });
+        let results = await cursor.toArray();
+        results = results[results.length - 1];
+        res.json(results);
+      } else {
+        res.send("something went wrong");
+      }
+    } else {
+      res.json("You must provide a valid API key in the request headers.");
+    }
+  });
 
 module.exports = router;
