c3431c4c2cba0b77b340cdeef067ee9345513c8f
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-06-12 15:04:37 +1000

    changed middleware

--- backend/server.js
+++ backend/server.js
@@ -14,25 +14,25 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use((req, res, next) => {
-  if (req.originalUrl === '/webhook') {
-    next(); // Do nothing with the body because I need it in a raw state.
-  } else {
-    express.json()(req, res, next);  // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.originalUrl === '/webhook') {
+//     next(); // Do nothing with the body because I need it in a raw state.
+//   } else {
+//     express.json()(req, res, next);  // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
+//   }
+// });
 
 
-// app.use(URLConfig.URL_API + '/webhooks-stripe', express.raw({ type: 'application/json' }), WebHooksRoutes)
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "client/build")));
-// app.use(
-//   express.json({
-//     verify: (req, res, buf) => {
-//       req.rawBody = buf;
-//     },
-//   })
-// );
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+  );
+app.use(URLConfig.URL_API + '/webhooks', express.raw({ type: 'application/json' }), WebHooksRoutes)
 app.use(routes);
 
 mongoConnection();
