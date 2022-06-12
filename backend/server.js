d42a5ee9fa4eaf67084c54c54a80494a3c733918
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-06-12 14:58:57 +1000

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use((req, res, next) => {
+  if (req.originalUrl === '/webhook') {
+    next(); // Do nothing with the body because I need it in a raw state.
+  } else {
+    express.json()(req, res, next);  // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
+  }
+});
+
+
 // app.use(URLConfig.URL_API + '/webhooks-stripe', express.raw({ type: 'application/json' }), WebHooksRoutes)
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "client/build")));
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf;
-    },
-  })
-);
+// app.use(
+//   express.json({
+//     verify: (req, res, buf) => {
+//       req.rawBody = buf;
+//     },
+//   })
+// );
 app.use(routes);
 
 mongoConnection();
